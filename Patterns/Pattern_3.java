e1e43e18817d15809f2743590cf2c93ff955cd1a
Trishit Hazra <trishitgithub@gmail.com>
2026-05-23 11:26:33 +0530

    Third pattern

---
+++ Patterns/Pattern_3.java
@@ -0,0 +1,26 @@
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+package Patterns;
+
+public class Pattern_3 {
+
+    public static void print(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        print(3);
+        print(4);
+        print(5);
+    }
+}
