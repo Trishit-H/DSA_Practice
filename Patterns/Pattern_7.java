bd051430434a0c93e9ef8f67575b2e5fe9e8e996
Trishit Hazra <trishitgithub@gmail.com>
2026-05-23 13:45:04 +0530

    Seventh pattern

---
+++ Patterns/Pattern_7.java
@@ -0,0 +1,35 @@
+//     *    
+//    ***   
+//   *****  
+//  ******* 
+// *********
+
+package Patterns;
+
+public class Pattern_7 {
+
+    public static void print(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        print(3);
+        print(4);
+        print(5);
+    }
+}
