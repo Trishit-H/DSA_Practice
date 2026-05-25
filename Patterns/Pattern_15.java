74d9edfcef8cc0497cef9bac4dc6f8c7b85f7eac
Trishit Hazra <trishitgithub@gmail.com>
2026-05-25 20:31:46 +0530

    Fifteenth Pattern

---
+++ Patterns/Pattern_15.java
@@ -0,0 +1,16 @@
+public class Pattern_15 {
+
+    public static void print(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
