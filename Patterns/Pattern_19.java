3f89de1072d85830b61f47ab72161e6577926f12
Trishit Hazra <trishitgithub@gmail.com>
2026-05-25 22:41:08 +0530

    Ninteenth Pattern

---
+++ Patterns/Pattern_19.java
@@ -0,0 +1,40 @@
+public class Pattern_19 {
+
+    public static void print(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < 2 * (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
