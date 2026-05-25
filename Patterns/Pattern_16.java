72c946adc220078a735c858a05693bd9fe8f7e36
Trishit Hazra <trishitgithub@gmail.com>
2026-05-25 20:41:31 +0530

    Sixteenth Pattern

---
+++ Patterns/Pattern_16.java
@@ -0,0 +1,16 @@
+public class Pattern_16 {
+
+    public static void print(int n) {
+        for (char ch = 'A'; ch < 'A' + n; ch++) {
+            for (int i = 1; i <= (ch - 'A' + 1); i++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
