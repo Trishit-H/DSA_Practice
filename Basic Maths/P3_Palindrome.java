d3873c4bca7a453fdb997c2c066c22a2c14884b2
Trishit Hazra <trishitgithub@gmail.com>
2026-05-26 11:51:49 +0530

    Program to check palindrome

---
+++ Basic Maths/P3_Palindrome.java
@@ -0,0 +1,29 @@
+public class P3_Palindrome {
+
+    public static boolean isPalindrome(int num) {
+        int reversed = 0;
+        int copy = num;
+
+        if (num < 0) {
+            return false;
+        }
+
+        if (num < 10) {
+            return true;
+        }
+
+        while (copy != 0) {
+            int lastDigit = copy % 10;
+            reversed = (reversed * 10) + lastDigit;
+            copy = copy / 10;
+        }
+
+        return (num == reversed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+}
