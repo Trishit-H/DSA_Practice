da120735841d6e764ae0567e56b2dfa994e1ebd3
Trishit Hazra <trishitgithub@gmail.com>
2026-05-26 10:53:55 +0530

    [Brute Force] Count digits in a number

---
+++ Basic Maths/P1_CountDigits_Brute.java
@@ -0,0 +1,18 @@
+public class P1_CountDigits_Brute {
+
+    public static int count(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n = n / 10;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(7789));
+        System.out.println(count(1000));
+    }
+}
