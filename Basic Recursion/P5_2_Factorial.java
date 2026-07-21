0ceaa7cd09ea8f548c74991213e69fc82d3f5c89
Trishit Hazra <trishitgithub@gmail.com>
2026-07-21 12:02:52 +0530

    [Recursion][Functional] Factorial

---
+++ Basic Recursion/P5_2_Factorial.java
@@ -0,0 +1,16 @@
+// Functional
+
+public class P5_2_Factorial {
+    public static void main(String[] args) {
+        int N = 5;
+        System.out.println(getFactorial(N));
+    }
+
+    public static int getFactorial(int N) {
+        if (N == 1) {
+            return 1;
+        }
+
+        return N * getFactorial(N - 1);
+    }
+}
