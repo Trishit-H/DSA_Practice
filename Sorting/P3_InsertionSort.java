d279aedbef1338e0e6486853cd88a36fb18faa19
Trishit Hazra <trishitgithub@gmail.com>
2026-08-02 18:01:18 +0530

    Insertion Sort algorithm

---
+++ Sorting/P3_InsertionSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class P3_InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+
+            int key = arr[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 5, 3, 4, 1, 2 };
+
+        insertionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
